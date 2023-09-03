2c649071e7602940e51bb19e2428853ab9fe17d9
basagoitiam <marika.basagoitia@gmail.com>
2023-09-03 18:09:21 -0400

    updated logic for handling admin access

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState, useEffect } from "react";
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
@@ -7,13 +8,30 @@ import BlogPost from './pages/BlogPost';
 
 function App() {
 
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/get-admin-token")
+    .then((res) => res.json())
+    .then((data) => {
+        const adminToken = data.adminToken;
+        const hasAdminAccess = window.location.href.includes(adminToken);
+        if (hasAdminAccess) {
+            setIsAdmin(true);
+        }
+    })
+    .catch((err) => {
+        console.error(err)
+    });
+  }, [isAdmin])
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<CreatePost />} />
-        <Route path="/update/:id" element={<UpdatePost />} />
-        <Route path="/:id" element={<BlogPost />} />
+        <Route path="/" element={<Home isAdmin={isAdmin} />} />
+        <Route path="/new" element={<CreatePost isAdmin={isAdmin} />} />
+        <Route path="/update/:id" element={<UpdatePost isAdmin={isAdmin} />} />
+        <Route path="/:id" element={<BlogPost isAdmin={isAdmin} />} />
       </Routes>
     </BrowserRouter>
   );
--- src/client/src/pages/Home.jsx
+++ src/client/src/pages/Home.jsx
@@ -4,29 +4,13 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Navigation from "../components/Navbar";
-import { Link, useHistory, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import PostCard from "../components/Card";
 
-function Home() {
+function Home({ isAdmin }) {
 
-    const history = useHistory();
 // problems here
-    const param1 = null;
-    const [isAdmin, setIsAdmin] = useState(false);
-
-    fetch("http://localhost:8080/api/get-admin-token")
-    .then((res) => res.json())
-    .then((data) => {
-        const adminToken = data.adminToken;
-        const hasAdminAccess = window.location.href.includes(adminToken);
-        if (hasAdminAccess) {
-            setIsAdmin(true);
-        }
-    })
-    .catch((err) => {
-        console.error(err)
-    });
-
+    
     const [blogPosts, setBlogPosts] = useState([]);
     
     useEffect(() => {
@@ -58,10 +42,10 @@ function Home() {
                 </Col>
             </Row>
             <Navigation />
-            { isAdmin? (
+            { isAdmin ? (
                 <Row>
                     <Col>
-                        <Link to="/new" className="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
+                        <Link to={"/new"} className="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
                         <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                         </svg>
                         </Link>
